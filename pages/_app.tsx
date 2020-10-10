13737f6f451eb95415786582965eaf56e92a3d22
Glenn Hevey <glenn.hevey@gmail.com>
2020-10-11 09:50:46 +1100

    Initial version

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import { AppProps } from "next/app";
+import React from "react";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <Component {...pageProps} />
+    )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Cade - Retro platform</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <meta name="msapplication-TileColor" content="#da532c" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1>Hello World</h1>
+            </main>
+
+            <footer className={styles.footer}>
+
+            </footer>
+        </div>
+    )
+}
