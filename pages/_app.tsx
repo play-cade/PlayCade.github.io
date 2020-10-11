a848b5695f10fa60187dd72364bf2445c73cd216
Glenn Hevey <glenn.hevey@gmail.com>
2020-10-11 22:58:33 +1100

    trying new build

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import { AppProps } from "next/app";
-import React from "react";
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <Component {...pageProps} />
-    )
-}
