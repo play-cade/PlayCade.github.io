69902489bb4ef1fb6e35bf9a7449cb3152be9d07
Glenn Hevey <glenn.hevey@gmail.com>
2020-10-12 17:22:26 +1100

    Added text to link to Github

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,23 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Cade - Retro Platform</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Cade - Retro Platform</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello World, Welcome to Cade
-        </h1>
-      </main>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Hello World, Welcome to Cade
+                </h1>
+                <p>Cade is currently in the early stages of development. Come hang on our <a href={"https://github.com/PlayCade"}>Github</a> to find out more</p>
+            </main>
 
-      <footer className={styles.footer}>
+            <footer className={styles.footer}>
 
-      </footer>
-    </div>
-  )
+            </footer>
+        </div>
+    )
 }
