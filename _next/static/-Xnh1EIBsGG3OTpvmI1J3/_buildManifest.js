8ad2e4859a02f2712e97ece989bd5daf911e0d4c
hevey <glenn.hevey@gmail.com>
2023-10-02 21:48:00 +0000

    Deploying to gh-pages from @ play-cade/PlayCade.github.io@3d2628734774d65d22beb66d5077dc9df19819ad 🚀

--- _next/static/-Xnh1EIBsGG3OTpvmI1J3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a5ba07be2c1b90ae.css","static/chunks/pages/index-034675ff8f670ef6.js"],"/_error":["static/chunks/pages/_error-92abceb22ce14966.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/Z24OkGN3NaI2rXk_8fBLs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a5ba07be2c1b90ae.css","static/chunks/pages/index-043bd0fb9cb29ed2.js"],"/_error":["static/chunks/pages/_error-92abceb22ce14966.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
